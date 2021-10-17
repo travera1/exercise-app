0c1444a7eea776f10d505f1f9b527abe113ea718
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-10-16 21:35:03 -0400

    changes to index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const port = process.env.PORT
 const express = require('express')
 const app = express()
+const serveStatic = require('serve-static')
 const path = require('path')
 const cors = require('cors')
 const morgan = require('morgan')
@@ -12,20 +13,21 @@ app.use(morgan('tiny'))
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//configure dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+//this * route is to serve project on different page routes except rouut '//
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-.get('/newpaltz', (req, res) => {
-  res.send('hello New paltz')
-});
 
-
-if (process.env.NODE_ENV == 'production') {
+/*if (process.env.NODE_ENV == 'production') {
   app.use(express.static('client/dist'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   })
 }
+*/
 
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`)
